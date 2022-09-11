556ee3c7e456d6c1864e81585763b24316b66fcd
Javier Minchala <javierminchala@gmail.com>
2022-09-11 12:37:32 -0400

    Updates

--- _nuxt/static/1645516009/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FMHFrontierGuide\u002F_nuxt\u002Fstatic\u002F1645516009",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FMHFrontierGuide\u002F",assetsPath:"\u002FMHFrontierGuide\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"be3fdcc9"}}}}(null));
---
+++ _nuxt/static/1662913963/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FMHFrontierGuide\u002F_nuxt\u002Fstatic\u002F1662913963",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FMHFrontierGuide\u002F",assetsPath:"\u002FMHFrontierGuide\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a18cce4b"}}}}(null));
